425fc10e12f087bf85e8d2df64af14e0c27971b9
berberman <1793913507@qq.com>
2019-08-07 19:39:43 +0800

    ?

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -17,42 +17,42 @@
 
 void opcontrol() {
     pros::Controller master(pros::E_CONTROLLER_MASTER);
-    
-    chassis_t   chassis;
-    arm_t       arm;
+
+    chassis_t chassis;
+    arm_t arm;
     collector_t collector;
-    
-    auto position_left_last         = .0,
-         position_right_last        = .0;
-    
-    odometry_t<>     odometry{};
+
+    auto position_left_last = .0,
+            position_right_last = .0;
+
+    odometry_t<> odometry{};
     chassis_config_t chassis_config = {
-        37.5,
-        5.0,
-        5.0
+            .375,
+            .05,
+            .05
     };
-    
-    #pragma clang diagnostic push
-    #pragma clang diagnostic ignored "-Wmissing-noreturn"
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-noreturn"
     while (true) {
-        pros::lcd::print(1, "left %d, right %d", chassis.left_position(), chassis.right_position());
+        pros::lcd::print(1, "left current %d, right current%d", chassis.left_position(), chassis.right_position());
         pros::lcd::print(2, "x %f, y %f, w %f", odometry.x, odometry.y, odometry.theta);
         pros::lcd::print(3, "s %f, a %f", odometry.s, odometry.a);
         pros::lcd::print(4, "arm %d", arm.current_position());
-        
-        auto a                      = master.get_analog(ANALOG_LEFT_Y),
-             b                      = master.get_analog(ANALOG_RIGHT_X);
-        auto position_left_current  = chassis.left_position(),
-             position_right_current = chassis.right_position();
-        
+
+        auto a = master.get_analog(ANALOG_LEFT_Y),
+                b = master.get_analog(ANALOG_RIGHT_X);
+        auto position_left_current = chassis.left_position(),
+                position_right_current = chassis.right_position();
+
         chassis.move(a, b);
         odometry += wheels_to_odometry(position_left_current - position_left_last,
                                        position_right_current - position_right_last,
                                        chassis_config);
-        pros::lcd::print(3, "d_left %f, d_right %f",
+        pros::lcd::print(5, "d_left %f, d_right %f",
                          position_left_current - position_left_last,
                          position_right_current - position_right_last);
-        
+
         if (master.get_digital(DIGITAL_L2)) {
             //up
         } else if (master.get_digital(DIGITAL_L1)) {
@@ -60,7 +60,7 @@ void opcontrol() {
         } else {
             //stop
         }
-        
+
         if (master.get_digital(DIGITAL_R2)) {
             collector.collect();
         } else if (master.get_digital(DIGITAL_R1)) {
@@ -68,10 +68,11 @@ void opcontrol() {
         } else {
             collector.stop();
         }
-        
-        position_left_last  = position_left_current;
+
+        position_left_last = position_left_current;
         position_right_last = position_right_current;
+
         pros::delay(20);
     }
-    #pragma clang diagnostic pop
+#pragma clang diagnostic pop
 }
